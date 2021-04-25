eb170518441e0866e19b567aa0c506956c650624
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-24 21:31:39 -0700

    Update stock_serializer.rb

--- app/serializers/stock_serializer.rb
+++ app/serializers/stock_serializer.rb
@@ -1,5 +1,5 @@
 class StockSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :price, :type, :vendor_id, :product_id
+  attributes :id, :quantity, :price, :type, :vendor_id, :product_id, :created_at
   has_one :product
   has_one :vendor
   # belongs_to :product, serializer: ProductSerializer
