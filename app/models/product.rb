7404f019bdd24ab17ab3ffbc9f417e2f913d904e
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-01-14 15:57:07 +0500

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
     validates :name, uniqueness: true, presence: true
     belongs_to :company
     has_many :stocks
+    has_many :transaction_records, dependent: :destroy
+    # has_one :transaction, through: :transaction_record
 end
